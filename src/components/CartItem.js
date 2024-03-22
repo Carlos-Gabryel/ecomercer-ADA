bfb79eff95fcdd52e0a915ef9f332febde67b7d2
N3th3rL0rd <hackerhel@gmail.com>
2024-03-22 19:21:31 -0300

    add cart, logic to add items on the cart and context to sidebar

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  const { id, title, image, price, amount } = item;
+  return (
+    <div className="flex">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        <div>
+          <Link to={`/product/${id}`}>
+            <img className="max-w-[80px]" src={image} alt="" />
+          </Link>
+          <div>
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
